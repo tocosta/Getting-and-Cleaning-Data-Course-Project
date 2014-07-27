17f19b1f697b69f624c59b827d9730492b4f5ac1
Costa, S. <sergiocosta.vc@outlook.com>
2014-07-27 01:35:10 +0100

    CodeBook.md

--- run_analysis.r
+++ run_analysis.r
@@ -1,33 +1,25 @@
-
-# Source of data for the project:
-# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+# Source of data for the project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 # You should create one R script called run_analysis.R that does the following. 
 # This R script does the following:
 
-  if (!require("data.table")) {
-    install.packages("data.table")
-  }
-  
-  require("data.table")
-  
-  # File to Download
+  # File URL to download
   fileURL <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
   
-  # Local Data File
+  # Local data file
   dataFileZIP <- "./getdata-projectfiles-UCI-HAR-Dataset.zip"
   
-  # Diretory Dataset
+  # Directory
   dirFile <- "./UCI HAR Dataset"
   
-  # Target csv Tidy Files
+  # Directory and filename (.csv) of the clean/tidy data
   tidyDataFileAVG <- "./tidy-UCI-HAR-AVG-dataset.csv"
   
-  # Data File downloading
+  # Download the dataset (.ZIP), which does not exist
   if (file.exists(dataFileZIP) == FALSE) {
       download.file(fileURL, destfile = dataFileZIP)
     }
   
-  # Uncompress Data File
+  # Uncompress data file
   if (file.exists(dirFile) == FALSE) {
     unzip(dataFileZIP)
   }
@@ -40,41 +32,43 @@
   subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
   subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
 
-  #combines data table (train vs test) by rows
+  # Combines data table (train vs test) by rows
   x <- rbind(x_train, X_test)
   y <- rbind(y_train, y_test)
   s <- rbind(subject_train, subject_test)
 
-  # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-  # read features labels
+  # 2. Extracts only the measurements on the mean and standard deviation for each measurement:
+  # Read features labels
   features <- read.table("./UCI HAR Dataset/features.txt")
-  # names to features column
+  # Friendly names to features column
   names(features) <- c('feat_id', 'feat_name')
-  # search for matches to argument mean or standard deviation (sd)  within each element of a character vector
+  # Search for matches to argument mean or standard deviation (sd)  within each element of character vector
   index_features <- grep("-mean\\(\\)|-std\\(\\)", features$feat_name) 
   x <- x[, index_features] 
-  # replaces all matches of a string features ("")
+  # Replaces all matches of a string features 
   names(x) <- gsub("\\(|\\)", "", (features[index_features, 2]))
 
-  # 3. Uses descriptive activity names to name the activities in the data set
-  # 4. Appropriately labels the data set with descriptive activity names.
+  # 3. Uses descriptive activity names to name the activities in the data set:
+  # 4. Appropriately labels the data set with descriptive activity names:
   # Read activity labels
   activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
-  # Names to activities column
+  # Friendly names to activities column
   names(activities) <- c('act_id', 'act_name')
   y[, 1] = activities[y[, 1], 2]
 
-  names(y) <- "Activity"
-  names(s) <- "Subject"
-
-  # combines data table by columns
+  # Combines data table by columns
   tidyDataSet <- cbind(s, y, x)
   
-  # 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+  # 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject:
   p <- tidyDataSet[, 3:dim(tidyDataSet)[2]] 
   tidyDataAVGSet <- aggregate(p,list(tidyDataSet$Subject, tidyDataSet$Activity), mean)
-
-  # created csv (tidy data set AVG) in Diretory
+  
+  # Activity and Subject name of columns 
+  names(tidyDataAVGSet)[1] <- "Subject"
+  names(tidyDataAVGSet)[2] <- "Activity"
+  
+  # Created csv (tidy data set AVG) in diretory
   write.csv(tidyDataAVGSet, tidyDataFileAVG)
 
-
+## NOTE: Read the file "Readme.md" and "Codebook.md" to understand the functioning of the "run_analysis.R". 
+## You can see how it works and how you can validate its operation.
